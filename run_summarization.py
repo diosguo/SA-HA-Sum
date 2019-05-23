542b8bb9072a23a7ed10f5f280d16bd5a302e616
theDoctor2013 <943024256@qq.com>
2019-05-23 18:48:03 +0800

    vocab added

--- run_summarization.py
+++ run_summarization.py
@@ -1,7 +1,7 @@
 # import something
 import tensorflow as tf
 import os
-
+from collections import namedtuple
 from Vocab import Vocab
 
 # define some flags
@@ -11,6 +11,7 @@ tf.app.flags.DEFINE_string('data_path','','Pa')
 tf.app.flags.DEFINE_string('log_root','','root dir to log')
 tf.app.flags.DEFINE_string('exp_name','','name of experiment')
 tf.app.flags.DEFINE_string('mode','train','mode of running')
+tf.app.flags.DEFINE_boolean('single_pass',False,'process data one by one or not')
 
 tf.app.flags.DEFINE_string('vocab_path','','path of vocab file')
 tf.app.flags.DEFINE_integer('vocab_size',5000,'Size of vocab')
@@ -35,6 +36,19 @@ def main(argv_unused):
             raise Exception('Logdir %s is not exists.' % FLAGS.log_root)
 
     vocab = Vocab(FLAGS.vocab_path, FLAGS.vocab_size)
+
+    hparam_list = ['mode','lr','adagrad_init_acc','rand_unif_init_mag','trunc_norm_init_std','max_grad_norm',
+                   'hidden_dim','emb_dim','batch_size','max_dec_steps','max_enc_steps','coverage','cov_loss_wt',
+                   'pointer_gen']
+
+    hps_dict = {}
+    for key,val in FLAGS.__flags.items():
+        if key in hparam_list:
+            hps_dict[key] = val
+
+    hps = namedtuple('HParams',hps_dict.keys())(**hps_dict)
+
+    batcher = Batcher(FLAGS.data_path, vocab, hps, single_pass=FLAGS.single_pass)
 
 
 
