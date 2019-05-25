c11ece49d38ad40d70e368cf1cfafdf554fd8d8e
theDoctor2013 <943024256@qq.com>
2019-05-25 20:43:00 +0800

    completed _mask_and_avg(),_coverage_loss()

--- model.py
+++ model.py
@@ -113,36 +113,69 @@ class SummarizationModel(object):
             self.rand_unif_init = tf.random_uniform_initializer(-hps.rand_unif_init_mag, hps.rand_unif_init_mag, seed=123)
             self.trunc_norm_init = tf.truncated_normal_initializer(stddev=hps.trunc_norm_init_std)
 
-        with tf.variable_scope('embedding'):
-            embedding = tf.get_variable('embedding',[vsize,hps.emb_dim], dtype=tf.float32, initializer=self.trunc_norm_init)
-            if hps.mode == 'train':
-                self._add_emb_vis(embedding)
-            emb_enc_inputs = tf.nn.embedding_lookup(embedding, self._enc_batch)
-            emb_dec_inputs = [tf.nn.embedding_lookup(embedding,x) for x in tf.unstack(self._dec_batch, axis=1)]
+            with tf.variable_scope('embedding'):
+                embedding = tf.get_variable('embedding',[vsize,hps.emb_dim], dtype=tf.float32, initializer=self.trunc_norm_init)
+                if hps.mode == 'train':
+                    self._add_emb_vis(embedding)
+                emb_enc_inputs = tf.nn.embedding_lookup(embedding, self._enc_batch)
+                emb_dec_inputs = [tf.nn.embedding_lookup(embedding,x) for x in tf.unstack(self._dec_batch, axis=1)]
 
-        enc_outputs, fw_st, bw_st = self._add_encoder(emb_enc_inputs, self._enc_lens)
-        self._enc_states = enc_outputs
+            enc_outputs, fw_st, bw_st = self._add_encoder(emb_enc_inputs, self._enc_lens)
+            self._enc_states = enc_outputs
 
-        self._dec_in_state = self._reduce_states(fw_st, bw_st)
+            self._dec_in_state = self._reduce_states(fw_st, bw_st)
 
-        with tf.variable_scope('decoder'):
-            decoder_outputs, self._dec_out_state, self.attn_dists, self.p_gens, self.coverage = self._add_decoder(emb_dec_inputs)
+            with tf.variable_scope('decoder'):
+                decoder_outputs, self._dec_out_state, self.attn_dists, self.p_gens, self.coverage = self._add_decoder(emb_dec_inputs)
 
-        with tf.variable_scope('output_projection'):
-            w = tf.get_variable('w',[hps.hidden_dim, vsize], dtype=tf.float32,initializer=self.trunc_norm_init)
-            w_t = tf.transpose(w)
-            v = tf.get_variable('v',[vsize],dtype=tf.float32,initializer=self.trunc_norm_init)
-            vocab_scores = []
-            for i,output in enumerate(decoder_outputs):
-                if i>0:
-                    tf.get_variable_scope().reuse_variables()
-                vocab_scores.append(tf.nn.xw_plus_b(output,w,v))
-            vocab_dists = [tf.nn.softmax(s) for s in vocab_scores]
+            with tf.variable_scope('output_projection'):
+                w = tf.get_variable('w',[hps.hidden_dim, vsize], dtype=tf.float32,initializer=self.trunc_norm_init)
+                w_t = tf.transpose(w)
+                v = tf.get_variable('v',[vsize],dtype=tf.float32,initializer=self.trunc_norm_init)
+                vocab_scores = []
+                for i,output in enumerate(decoder_outputs):
+                    if i>0:
+                        tf.get_variable_scope().reuse_variables()
+                    vocab_scores.append(tf.nn.xw_plus_b(output,w,v))
+                vocab_dists = [tf.nn.softmax(s) for s in vocab_scores]
 
-        if FLAGS.pointer_gen:
-            final_dists = self._calc_final_dist(vocab_dists, self.attn_dists)
-        else:
-            final_dists = vocab_dists
+            if FLAGS.pointer_gen:
+                final_dists = self._calc_final_dist(vocab_dists, self.attn_dists)
+            else:
+                final_dists = vocab_dists
+
+
+            if hps.mode in ['train','eval']:
+                with tf.variable_scope('loss'):
+                    if FLAGS.pointer_gen:
+                        loss_per_step = []
+                        batch_nums = tf.range(0,limit=hps.batch_size)
+                        for dec_step, dist in enumerate(final_dists):
+                            targets = self._target_batch[:,dec_step]
+                            indices = tf.stack((batch_nums,targets),axis=1)
+                            gold_probs = tf.gather_nd(dist, indices)
+                            losses = -tf.log(gold_probs)
+                            loss_per_step.append(losses)
+
+                        self._loss = _mask_and_avg(loss_per_step, self._dec_padding_mask)
+
+                    else:
+                        self._loss = contrib.seq2seq.sequence_loss(tf.stack(vocab_scores,axis=1),self._target_batch,self._dec_padding_mask)
+
+
+                    tf.summary.scalar('loss', self._loss)
+
+                    if hps.coverage:
+                        with tf.variable_scope('coverage_loss'):
+                            self._coverage_loss = _coverage_loss(self.attn_dists, self._dec_padding_mask)
+                            tf.summary.scalar('coverage_loss', self._coverage_loss)
+                        self._total_loss = self._loss + hps.cov_loss_wt*self._coverage_loss
+                        tf.summary.scalar('total_loss',self._total_loss)
+        if hps.mode == 'decode':
+            assert len(final_dists) == 1
+            final_dists = final_dists[0]
+            topk_probs, self._topk_ids = tf.nn.top_k(final_dists, hps.batch_size*2)
+            self._topk_log_probs = tf.log(topk_probs)
 
 
 
@@ -154,3 +187,20 @@ class SummarizationModel(object):
         with tf.device('/gpu:0'):
             self._add_seq2seq()
         self.global_step = tf.Variable(0,name='global_step',trainable=False)
+
+
+def _mask_and_avg(values, padding_mask):
+    dec_lens = tf.reduce_sum(padding_mask, axis=1)
+    values_per_step = [v*padding_mask[:,dec_step] for dec_step,v in enumerate(values)]
+    values_per_ex = sum(values_per_step)/dec_lens
+    return tf.reduce_mean(values_per_ex)
+
+def _coverage_loss(attn_dists, padding_mask):
+    coverage = tf.zeros_like(attn_dists[0])
+    covlosses = []
+    for a in attn_dists:
+        covloss = tf.reduce_sum(tf.minimum(a,coverage),[1])
+        covlosses.append(covloss)
+        coverage += a
+    coverage_loss = _mask_and_avg(covlosses, padding_mask)
+    return coverage_loss
