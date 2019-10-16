8d95089a3d9cdc8219212e32efff515a11f32406
xuyang <943024256@qq.com>
2019-10-16 11:34:29 +0800

    添加了将句法分析树计算成句子向量的函数emb_tree

--- parse_summ/parse_summarization_model/model.py
+++ parse_summ/parse_summarization_model/model.py
@@ -1,6 +1,44 @@
 from mxnet import nd
 from mxnet.gluon import Block
+from mxnet.gluon import nn
+from mxnet import nd
 from stanfordcorenlp import StanfordCoreNLP
+from parse_parse import parse
+
+class ParseEncoder(Block):
+    def __init__(self, tag_emb, word_emb):
+        super(ParseEncoder, self).__init__()
+        self.tag_embedding = tag_emb
+        self.word_embedding = word_emb
+        self.word_ass = nn.Dense(200)
+
+    def emb_tree(self, root):
+        next_emb = None
+        if isinstance(root.next, str):
+            # Leaf Node
+            next_emb = self.word_embedding(root.next)
+        elif isinstance(root.next, list):
+            # Mid Node
+            next_emb = self.emb_tree(root.next[0])
+            for i in root.next[1:]:
+                next_emb += self.emb_tree(i)
+            next_emb /= len(root.next)
+        else:
+            # Wrong Node
+            raise Exception('Error with Parse Tree Node.next type'+str(type(root.nexts)))
+
+        return self.word_ass(nd.concat(self.tag_embedding(root.value), next_emb))
+
+    def forward(self, root):
+        pass
+
+
+class BaseModel(object):
+    def __init__(self):
+        self.tag_embedding = nn.Embedding(10,100)
+        self.word_embedding = nn.Embedding(20000,100)
+        params = [self.tag_embedding, self.word_embedding]
+        self.encoder = ParseEncoder(*params)
 
 
 class Model(object):
@@ -12,3 +50,6 @@ class Model(object):
         self.summary_path = original_path
         self.model_params = model_params
         self.mode = mode
+
+output = parse('(ROOT HELLO)')
+print(output.val, output.next)
