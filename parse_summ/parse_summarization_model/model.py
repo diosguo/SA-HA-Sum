c424a51d5f979c9b8ee7ded333babcfcdbb385b6
xuyang <943024256@qq.com>
2019-10-16 14:05:47 +0800

    little

--- parse_summ/parse_summarization_model/model.py
+++ parse_summ/parse_summarization_model/model.py
@@ -5,6 +5,7 @@ from mxnet import nd
 from stanfordcorenlp import StanfordCoreNLP
 from parse_parse import parse
 
+
 class ParseEncoder(Block):
     def __init__(self, tag_emb, word_emb):
         super(ParseEncoder, self).__init__()
@@ -25,7 +26,7 @@ class ParseEncoder(Block):
             next_emb /= len(root.next)
         else:
             # Wrong Node
-            raise Exception('Error with Parse Tree Node.next type'+str(type(root.nexts)))
+            raise Exception('Error with Parse Tree Node.next type' + str(type(root.nexts)))
 
         return self.word_ass(nd.concat(self.tag_embedding(root.value), next_emb))
 
@@ -35,8 +36,8 @@ class ParseEncoder(Block):
 
 class BaseModel(object):
     def __init__(self):
-        self.tag_embedding = nn.Embedding(10,100)
-        self.word_embedding = nn.Embedding(20000,100)
+        self.tag_embedding = nn.Embedding(10, 100)
+        self.word_embedding = nn.Embedding(20000, 100)
         params = [self.tag_embedding, self.word_embedding]
         self.encoder = ParseEncoder(*params)
 
@@ -44,12 +45,12 @@ class BaseModel(object):
 class Model(object):
 
     def __init__(self, stanford_path, mode, original_path, summary_path, model_params):
-
         nlp = StanfordCoreNLP(stanford_path)
         self.original_path = original_path
         self.summary_path = original_path
         self.model_params = model_params
         self.mode = mode
+
 
 output = parse('(ROOT HELLO)')
 print(output.val, output.next)
