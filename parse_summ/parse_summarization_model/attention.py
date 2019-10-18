6c22febc19a2e1437209e22434fec7e5bf5ed33e
xuyang <943024256@qq.com>
2019-10-18 11:00:26 +0800

    add bahanauAttention

---
+++ parse_summ/parse_summarization_model/attention.py
@@ -0,0 +1,24 @@
+from mxnet.gluon import nn
+from mxnet import nd
+
+class BahdanauAttention(nn.Block):
+    def __init__(self, units):
+        super(BahdanauAttention, self).__init__()
+        self.W1 = nn.Dense(units)
+        self.W2 = nn.Dense(units)
+        self.V = nn.Dense(1)
+
+    def forward(self, query, values):
+
+        hidden_with_time_axis = nd.expand_dims(query, 1)
+
+        score = self.V(nd.tanh(
+            self.W1(values) + self.W2(hidden_with_time_axis)
+        ))
+
+        attention_weights = nd.softmax(score, axis=1)
+
+        context_vector = attention_weights * values
+        context_vector = nd.sum(context_vector, axis=1)
+
+        return context_vector, attention_weights
--- parse_summ/parse_summarization_model/decoder.py
+++ parse_summ/parse_summarization_model/decoder.py
@@ -6,5 +6,5 @@ class Decoder(nn.Block):
         super(Decoder, self).__init__()
         cell = rnn.LSTMCell(100)
 
-    def forward(self, *args):
+    def forward(self, inputs, encoder_states):
         pass
