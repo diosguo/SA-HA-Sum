4dad70acc1bc2d621114b6f069ae9d495d36d61e
theDoctor2013 <943024256@qq.com>
2019-11-21 14:58:55 -0500

    update  lda and tfidf model

--- preprocess/get_headline_lda.py
+++ preprocess/get_headline_lda.py
@@ -2,6 +2,7 @@ from sklearn.decomposition import LatentDirichletAllocation
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pickle
 import os
+from tqdm import tqdm
 
 
 n_topics = 32
@@ -10,20 +11,29 @@ tfidf_vectorizer = TfidfVectorizer(min_df=2)
 
 lda_model = LatentDirichletAllocation(n_components=n_topics, max_iter=10)
 
-file_list = os.path.join('../data/cnn_headline/')
-datas = []
-for filename in file_list:
-    with open(os.path.join('../data/cnn_headline/', filename), 'r') as f:
-        datas.append(f.readline())
+path_to_headlines = '/home/xuyang/data/headlines'
 
+file_list = os.listdir(path_to_headlines)
+datas = []
+for filename in tqdm(file_list):
+    with open(os.path.join(path_to_headlines, filename), 'r', encoding='windows-1253') as f:
+        try:
+            datas.append(f.readline())
+        except UnicodeDecodeError as u:
+            print(filename)
+            raise u
+
+# fit tfidf
 tfidf_mat = tfidf_vectorizer.fit_transform(datas)
+
+# fit lda_model
 lda_mat = lda_model.fit_transform(tfidf_mat)
 
 pickle.dump(tfidf_mat, open('../data/tfidf_model.pkl','wb'))
 pickle.dump(lda_model, open('../data/lda_model.pkl','wb'))
 
 for idx, filename in enumerate(file_list):
-    with open(os.path.join('../data/cnn_head_lda/', filename),'wb') as f:
+    with open(os.path.join('/home/xuyang/data/headline_lda', filename),'wb') as f:
         pickle.dump(lda_mat[idx].tolist(), f)
     
 
